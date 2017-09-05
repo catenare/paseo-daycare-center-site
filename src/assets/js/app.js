675cc910b523ee593155a4cc45f183c3719f12f2
Johan Martin <martin.johan@johan-martin.com>
2017-09-05 22:00:58 +0200

    update about section

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,13 +1,20 @@
 import $ from 'jquery';
 import whatInput from 'what-input';
 
+let WebFont  = require('webfontloader');
+
 window.$ = $;
 
-import Foundation from 'foundation-sites';
+// import Foundation from 'foundation-sites';
 
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
-//import './lib/foundation-explicit-pieces';
+import './lib/foundation-explicit-pieces';
 
 
 $(document).foundation();
+WebFont.load({
+  google: {
+    families: ['Assistant', 'Rubik Mono One', 'Short Stack']
+  }
+});
