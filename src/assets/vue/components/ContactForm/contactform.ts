3ec86d635ca06072aef4c8ccd3598ff4016605c1
Johan Martin <martin.johan@johan-martin.com>
2017-09-15 01:06:40 +0200

    adding tsconfig.json file

--- src/assets/vue/components/ContactForm/contactform.ts
+++ src/assets/vue/components/ContactForm/contactform.ts
@@ -2,22 +2,27 @@ export default {
 
     data: function () {
         return {
+            order: -1,
             stories: [
                 {
                     plot: "I crased my car today",
-                    writer: "Alex"
+                    writer: "Alex",
+                    upvotes: 28
                 },
                 {
                     plot: "Yesterday, someone stole my bag!",
-                    writer: "John"
+                    writer: "John",
+                    upvotes: 8
                 },
                 {
                     plot: "Someone ate my chocolate...",
-                    writer: "John"
+                    writer: "John",
+                    upvotes: 51
                 },
                 {
                     plot: "I ate someone's chocolate",
-                    writer: "Alex"
+                    writer: "Alex",
+                    upvotes: 74
                 }
             ]
         }
